aac28dfd0952ab20d5aaaff39e5148b0863bf8ed
masopego <masopego@gmail.com>
2025-02-23 18:04:25 +0100

    Bublble position draft

--- app/src/main/java/dam/pmdm/spyrothedragon/MainActivity.java
+++ app/src/main/java/dam/pmdm/spyrothedragon/MainActivity.java
@@ -1,8 +1,17 @@
 package dam.pmdm.spyrothedragon;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -12,28 +21,48 @@ import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+
 import dam.pmdm.spyrothedragon.databinding.ActivityMainBinding;
+import dam.pmdm.spyrothedragon.databinding.FragmentCharactersGuideBinding;
+import dam.pmdm.spyrothedragon.databinding.FragmentWelcomeBinding;
+import dam.pmdm.spyrothedragon.databinding.FragmentWorldsGuideBinding;
+import dam.pmdm.spyrothedragon.utils.ViewAnimator;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     NavController navController = null;
 
+    private FragmentWelcomeBinding welcomeBinding;
+    private FragmentCharactersGuideBinding charactersGuideBinding;
+    private FragmentWorldsGuideBinding worldsGuideBinding;
+
+    private Boolean needToStartGuide = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        welcomeBinding = binding.includeLayout;
+        charactersGuideBinding = binding.includeCharacterLayout;
+        worldsGuideBinding = binding.includeWorldsLayout;
+
         setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbar);
 
         Fragment navHostFragment = getSupportFragmentManager().findFragmentById(R.id.navHostFragment);
         if (navHostFragment != null) {
             navController = NavHostFragment.findNavController(navHostFragment);
             NavigationUI.setupWithNavController(binding.navView, navController);
             NavigationUI.setupActionBarWithNavController(this, navController);
+
         }
 
         binding.navView.setOnItemSelectedListener(this::selectedBottomMenu);
+
 
         navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
             if (destination.getId() == R.id.navigation_characters ||
@@ -48,7 +77,97 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        initializeGuide();
+
     }
+
+    private void initializeGuide() {
+        if (needToStartGuide){
+            initializeWelcomeStep();
+        }
+    }
+
+    private void initializeWelcomeStep() {
+        welcomeBinding.exitGuide.setOnClickListener(this::onExitGuide);
+        welcomeBinding.guideLayout.setVisibility(View.VISIBLE);
+
+        ImageView spyroImage = welcomeBinding.spyroTitleImage;
+        ImageButton btnStart = welcomeBinding.btnStart;
+
+        // Create animations
+        ViewAnimator.animateTranslationX(spyroImage, -50f, 50f, 3000);
+        ViewAnimator.animateTranslationY(btnStart, -15f, 15f, 1000);
+
+        welcomeBinding.btnStart.setOnClickListener(v ->initializeNavigationGuide());
+    }
+
+    private void initializeNavigationGuide() {
+        welcomeBinding.guideLayout.setVisibility(View.GONE);
+        charactersGuideBinding.exitGuide.setOnClickListener(this::onExitGuide);
+        charactersGuideBinding.characterGuideLayout.setVisibility(View.VISIBLE);
+
+        ImageView bubble = charactersGuideBinding.bubble;
+        View navigationView = binding.navView;
+
+
+
+
+        bubble.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                // Obtener el ancho de la pantalla
+                DisplayMetrics displayMetrics = new DisplayMetrics();
+                getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+                int screenWidth = displayMetrics.widthPixels;
+                int heightPixels = displayMetrics.heightPixels;
+// 1070
+// Calcular el ancho de la burbuja
+                int bubbleWidth = v.getWidth();
+                int menuButtonWidth = screenWidth / 3;
+
+                int firstPosition = (menuButtonWidth/2) - bubbleWidth/2;
+                int secondPosition = menuButtonWidth+(menuButtonWidth/2) -bubbleWidth/2;
+                int thirdPosition = menuButtonWidth*2+(menuButtonWidth/2)- bubbleWidth/2;
+                // Elimina el listener si solo necesitas obtenerlo una vez
+                bubble.removeOnLayoutChangeListener(this);
+
+                float bubbleX = secondPosition; // O usa firstPosition o thirdPosition según sea necesario
+
+// La burbuja sigue estando en la parte inferior, solo ajustamos X.
+                bubble.setX(bubbleX);
+                bubble.setY(heightPixels-(bubble.getHeight()/3*2));
+            }
+        });
+
+        // Configurar botón "Siguiente"
+        charactersGuideBinding.btnNext.setOnClickListener(v -> initializeNavigationGuide2());
+    }
+
+    private int[] getViewPosition(View view) {
+        int[] location = new int[2];
+        view.getLocationOnScreen(location);
+        return location;
+    }
+
+    private void initializeNavigationGuide2() {
+        navController.navigate(R.id.navigation_worlds);
+        charactersGuideBinding.characterGuideLayout.setVisibility(View.GONE);
+        worldsGuideBinding.worldsGuideLayout.setVisibility(View.VISIBLE);
+        worldsGuideBinding.btnNext.setOnClickListener(v->initializeNavigationGuide3());
+    }
+
+    private void initializeNavigationGuide3() {
+        worldsGuideBinding.worldsGuideLayout.setVisibility(View.GONE);
+        navController.navigate(R.id.navigation_collectibles);
+    }
+
+    private void onExitGuide(View view) {
+        needToStartGuide = false;
+        welcomeBinding.guideLayout.setVisibility(View.GONE);
+        charactersGuideBinding.characterGuideLayout.setVisibility(View.GONE);
+        worldsGuideBinding.worldsGuideLayout.setVisibility(View.GONE);
+    }
+
 
     private boolean selectedBottomMenu(@NonNull MenuItem menuItem) {
         if (menuItem.getItemId() == R.id.nav_characters)
